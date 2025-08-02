53ae6c34ed99a0d6d5fd681d2391618b3bc377d9
Your Name <you@example.com>
2025-08-02 23:19:03 +0800

    init: 🎉

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,27 @@
-import { defineConfig, loadEnv } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { createHtmlPlugin } from 'vite-plugin-html';
-import vueJsx from '@vitejs/plugin-vue-jsx';
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 // import eslintPlugin from 'vite-plugin-eslint'; //导入包
-import vueSetupExtend from 'vite-plugin-vue-setup-extend-plus';
-import autoImports from 'unplugin-auto-import/vite';
+import vueSetupExtend from 'vite-plugin-vue-setup-extend-plus'
+import autoImports from 'unplugin-auto-import/vite'
 // import { VitePWA } from 'vite-plugin-pwa';
-require('events').EventEmitter.defaultMaxListeners = 0  // 设置最大监听器数量为15
+require('events').EventEmitter.defaultMaxListeners = 0 // 设置最大监听器数量为15
 type CacheStrategy =
   | 'CacheFirst'
   | 'CacheOnly'
   | 'NetworkFirst'
   | 'NetworkOnly'
-  | 'StaleWhileRevalidate';
+  | 'StaleWhileRevalidate'
 interface IgetCache {
-  name: string;
-  pattern: RegExp | string;
-  cacheDay?: number;
-  cacheType?: CacheStrategy;
+  name: string
+  pattern: RegExp | string
+  cacheDay?: number
+  cacheType?: CacheStrategy
 }
 
-const autoprefixer = require('autoprefixer');
-const path = require('path');
+const autoprefixer = require('autoprefixer')
+const path = require('path')
 const getCache = ({ name, pattern, cacheDay = 7, cacheType }: IgetCache) => ({
   urlPattern: pattern,
   handler: cacheType || 'CacheFirst',
@@ -29,18 +29,22 @@ const getCache = ({ name, pattern, cacheDay = 7, cacheType }: IgetCache) => ({
     cacheName: name,
     expiration: {
       maxEntries: 500,
-      maxAgeSeconds: 60 * 60 * 24 * cacheDay,
+      maxAgeSeconds: 60 * 60 * 24 * cacheDay
     },
     cacheableResponse: {
-      statuses: [200],
-    },
-  },
-});
+      statuses: [200]
+    }
+  }
+})
 
 const config = ({ mode }) => {
   // const isProd = mode === 'production';
-  const envPrefix = 'APP_';
-  const { APP_TITLE = '' } = loadEnv(mode, process.cwd(), envPrefix);
+  const envPrefix = 'APP_'
+  const {
+    APP_TITLE = '',
+    APP_REPO = '',
+    APP_FONT_CSS_FILE = ''
+  } = loadEnv(mode, process.cwd(), envPrefix)
   return {
     // base: isProd ? '/vue-su-fabric-editor/' : './',
     base: './',
@@ -49,8 +53,8 @@ const config = ({ mode }) => {
       autoImports({
         imports: ['vue'],
         eslintrc: {
-          enabled: true,
-        },
+          enabled: true
+        }
       }),
       vueSetupExtend(),
       // 增加下面的配置项,这样在运行时就能检查eslint规范
@@ -65,9 +69,13 @@ const config = ({ mode }) => {
         inject: {
           data: {
             title: APP_TITLE,
-          },
-        },
-      }),
+            APP_TITLE,
+            APP_REPO,
+            APP_FONT_CSS_FILE,
+            BASE_URL: './'
+          }
+        }
+      })
     ],
     build: {
       target: 'es2015',
@@ -77,13 +85,13 @@ const config = ({ mode }) => {
       // sourcemap: !isProd,
       emptyOutDir: true,
       rollupOptions: {
-        input: path.resolve(__dirname, 'index.html'),// 入口文件
+        input: path.resolve(__dirname, 'index.html'), // 入口文件
         // 指定输出（打包）文件的目录
         output: {
           chunkFileNames: 'js/[name].[hash].js',
-          entryFileNames: 'js/[name].[hash].js',
-        },
-      },
+          entryFileNames: 'js/[name].[hash].js'
+        }
+      }
     },
     envPrefix,
     // 用来设置引用模块
@@ -91,20 +99,32 @@ const config = ({ mode }) => {
       alias: [
         { find: /^@\//, replacement: path.resolve(__dirname, 'src') + '/' },
         { find: /^~/, replacement: '' },
-        { find: /^vue-i18n/, replacement: 'vue-i18n/dist/vue-i18n.cjs.js' },
+        { find: /^vue-i18n/, replacement: 'vue-i18n/dist/vue-i18n.cjs.js' }
       ],
-      extensions: ['.ts', '.tsx', '.js', '.mjs', '.vue', '.json', '.less', '.css'],
+      extensions: [
+        '.ts',
+        '.tsx',
+        '.js',
+        '.mjs',
+        '.vue',
+        '.json',
+        '.less',
+        '.css'
+      ]
+    },
+    optimizeDeps: {
+      exclude: ['fsevents']
     },
     css: {
       postcss: {
-        plugins: [autoprefixer],
+        plugins: [autoprefixer]
       },
       preprocessorOptions: {
         less: {
-          javascriptEnabled: true,
+          javascriptEnabled: true
           // additionalData: `@import "${path.resolve(__dirname, 'src/styles/variable.less')}";`,
-        },
-      },
+        }
+      }
     },
     server: {
       port: 3000,
@@ -113,14 +133,14 @@ const config = ({ mode }) => {
         '/fontFile': {
           target: 'https://github.com/',
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/fontFile/, ''),
-        },
-      },
+          rewrite: path => path.replace(/^\/fontFile/, '')
+        }
+      }
     },
     preview: {
-      port: 5000,
-    },
-  };
-};
+      port: 5000
+    }
+  }
+}
 
-export default defineConfig(config);
+export default defineConfig(config)
